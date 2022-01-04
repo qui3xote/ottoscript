599eab89025f993cc4c9779f539f8390d4c14c18
qui3xote <tmellis@gmail.com>
2022-01-04 09:57:42 -0500

    Moved OttoScript into __init__ Updated ottoscript to handle execution logic.

--- __init__.py
+++ __init__.py
@@ -4,4 +4,39 @@ from ottolib.commands import *
 from ottolib.teststrings import *
 from ottolib.conditions import *
 from ottolib.triggers import *
-from ottolib.parser import *
+
+
+class OttoScript:
+    command = Or([cls.parser() for cls in BaseCommand.__subclasses__()])
+    trigger = Or([cls.parser() for cls in BaseTrigger.__subclasses__()])
+    condition = Or([cls.parser() for cls in BaseCondition.__subclasses__()])
+
+    WHEN, THEN = map(CaselessKeyword, ["WHEN", "THEN"])
+
+    when_expr = WHEN.suppress() + Group(trigger)("when")
+    then_clause = THEN.suppress() + Group(OneOrMore(command))("actions")
+    conditionclause = condition("conditions") + then_clause
+    _parser = when_expr + OneOrMore(Group(conditionclause))("condition_clauses")
+
+    def __init__(self, interpreter, script):
+        BaseVocab.set_interpreter(interpreter)
+        self._parsobj = self.parse(script)
+
+    @property
+    def parser(self):
+        return self._parser
+
+    def parse(self, script):
+        return self.parser.parse_string(script)
+
+    @property
+    def trigger(self):
+        return self._parsobj.when[0]
+
+    def execute(self):
+        for conditions, commands in self.condition_clauses.as_list():
+            if conditions.eval() == True:
+                for command in commands:
+                    command.eval()
+            else:
+                self.interpreter.log_info(f"Condition Failed: {condition}")
