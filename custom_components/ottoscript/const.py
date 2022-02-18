e0ad08e86e67e3c7b689cdbf832026b3b512154e
qui3xote <tmellis@gmail.com>
2022-02-18 06:46:12 -0500

    initial config steps

--- custom_components/ottoscript/const.py
+++ custom_components/ottoscript/const.py
@@ -1,1 +1,9 @@
-DOMAIN = "ottoscript"
+NAME = "Otto"
+DOMAIN = "otto"
+DOMAIN_DATA = f"{DOMAIN}_data"
+
+PYSCRIPT_FOLDER = "pyscript"
+PYSCRIPT_APP_FOLDER = PYSCRIPT_FOLDER + '/apps'
+
+# Otto Config Options
+CONFIG_SCRIPT_DIR = "user_script_directory"
